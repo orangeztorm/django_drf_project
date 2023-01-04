ff460de2c08b0e7b76cde93ae14167cfb2796436
Taiwo Kenny <taiwokenny45@gmail.com>
2023-01-04 17:25:07 +0100

    function based view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import Http404
 
 
 from .models import Product
@@ -38,3 +42,32 @@ class ProductListAPIView(generics.ListAPIView):
 
 
 product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        
+        # urls
+        # get single product
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            serializer = ProductSerializer(obj, many = False).data
+            return Response(serializer)
+        # get all products list view
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True).data
+        return Response(serializer)
+
+    if method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response(serializer.errors)
