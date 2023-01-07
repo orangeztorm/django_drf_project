b9ca087c4a579afcbf9e309b3f7d2da9f00d72c0
Taiwo Kenny <taiwokenny45@gmail.com>
2023-01-07 13:23:39 +0100

    custom permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .permissions import IsStaffEditiorPermission
 from django.http import Http404
 
 
@@ -13,7 +14,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAdminUser,IsStaffEditiorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
