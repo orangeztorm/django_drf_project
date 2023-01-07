17d65d07aad318cfc0f16653dce0f5ceb1b7a45b
Taiwo Kenny <taiwokenny45@gmail.com>
2023-01-07 11:38:13 +0100

    permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import Http404
@@ -12,6 +12,8 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
@@ -69,61 +71,61 @@ product_delete_view = ProductDeleteAPIView.as_view()
 
 
 
-class ProddctMixinView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProddctMixinView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-    def get(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-        if pk is not None:
-            return self.retrieve(request, *args, **kwargs);
-        return self.list(request, *args, **kwargs);
+#     def get(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk')
+#         if pk is not None:
+#             return self.retrieve(request, *args, **kwargs);
+#         return self.list(request, *args, **kwargs);
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs);
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs);
 
-    def perform_create(self, serializer):
-        # serializer.save(user=self.request.user)
-        print(serializer.validated_data)
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or None
-        if content is None:
-            content = title
-        serializer.save(content=content)
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         print(serializer.validated_data)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
+#         if content is None:
+#             content = title
+#         serializer.save(content=content)
 
-    def perform_update(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+#     def perform_update(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
 
-    def perform_destroy(self, instance):
-        return self.destroy(request, *args, **kwargs)
+#     def perform_destroy(self, instance):
+#         return self.destroy(request, *args, **kwargs)
         
 
 
 
-@api_view(['GET', 'POST'])
-def product_alt_view(request, pk=None, *args, **kwargs):
-    method = request.method
+# @api_view(['GET', 'POST'])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method = request.method
 
-    if method == 'GET':
+#     if method == 'GET':
         
-        # urls
-        # get single product
-        if pk is not None:
-            obj = get_object_or_404(Product, pk=pk)
-            serializer = ProductSerializer(obj, many = False).data
-            return Response(serializer)
-        # get all products list view
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True).data
-        return Response(serializer)
+#         # urls
+#         # get single product
+#         if pk is not None:
+#             obj = get_object_or_404(Product, pk=pk)
+#             serializer = ProductSerializer(obj, many = False).data
+#             return Response(serializer)
+#         # get all products list view
+#         queryset = Product.objects.all()
+#         serializer = ProductSerializer(queryset, many=True).data
+#         return Response(serializer)
 
-    if method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content') or None
-            if content is None:
-                content = title
-            serializer.save(content=content)
-            return Response(serializer.data)
-        return Response(serializer.errors)
+#     if method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             title = serializer.validated_data.get('title')
+#             content = serializer.validated_data.get('content') or None
+#             if content is None:
+#                 content = title
+#             serializer.save(content=content)
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
