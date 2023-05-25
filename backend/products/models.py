dd74059b273bc2f3a5624f3e8995d4f92eb9a0e7
Taiwo Kenny <taiwokenny45@gmail.com>
2023-05-25 19:54:59 +0100

    search

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,34 @@
 from decimal import Decimal
 from django.db import models
 from django.conf import settings
+from django.db.models.query import QuerySet
+from django.db.models import Q
 
 # Create your models here.
 
 user = settings.AUTH_USER_MODEL  # 'auth.User'
+
+
+class ProductQuerySet(models.QuerySet):
+    def is_pubic(self):
+        return self.filter(public=True)
+
+    def search(self, query):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_pubic().filter(lookup)
+        if user is not None:
+            qs2 = qs.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+
+class ProductManager(models.Manager):
+
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset.is_public().search(query, user)
 
 
 class Product(models.Model):
@@ -15,6 +39,9 @@ class Product(models.Model):
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(
         decimal_places=2, max_digits=15, default=Decimal('99.99'))
+    public = models.BooleanField(default=True)
+    
+    objects  = ProductManager()
 
     @property
     def sale_price(self):
